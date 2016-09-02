6b463ea3c8e9d06f7a5913dc9458c59746fc35b6
SeqSEE <sequoia.francois@gmail.com>
2016-09-01 23:21:19 -0700

    remove unused imports
    remove unused imports

--- src/main/java/com/shruglabs/hempfarmer/item/HFItem.java
+++ src/main/java/com/shruglabs/hempfarmer/item/HFItem.java
@@ -3,13 +3,7 @@ package com.shruglabs.hempfarmer.item;
 import com.shruglabs.hempfarmer.creativetab.HFCreativeTabs;
 import com.shruglabs.hempfarmer.init.HFItems;
 
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.world.World;
 
 public class HFItem extends Item {
 
