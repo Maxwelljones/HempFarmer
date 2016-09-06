0132b9ec16e6452c91262f4ddfcb7dbb5d79f3d7
SeqSEE <sequoia.francois@gmail.com>
2016-09-06 14:01:26 -0700

    update latest

--- src/main/java/com/shruglabs/hempfarmer/Reference.java
+++ src/main/java/com/shruglabs/hempfarmer/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	public static final String UPDATE = "https://raw.githubusercontent.com/MinecraftModDevelopment/HempFarmer/master/update.json";
 	public static final String ID = "hempfarmer";
 	public static final String NAME = "HempFarmer";
-	public static final String VER = "1.7.2";
+	public static final String VER = "1.7.3";
 	public static final String CLIENT_PROXY = "com.shruglabs.hempfarmer.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.shruglabs.hempfarmer.proxy.CommonProxy";
 	
